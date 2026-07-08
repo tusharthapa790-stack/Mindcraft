3fb475e2063dccff4812aeb878f662d3efa56e52
Tushar Thapa <tusharthapa790@gmail.com>
2026-07-08 16:11:31 +0545

    day3

---
+++ React/week1/day3.js
@@ -0,0 +1,40 @@
+// function multiply(a, b){
+//     return a * b
+// }
+// console.log(multiply(5, 10));
+
+// console.log(greet("Tuhsar"))
+// function greet(name){
+//     return `Hello ${name}`
+// }
+
+// const addition = (x, y, z) => {
+//     return x + y + z;
+// }
+// console.log(addition(3, 4, 5));
+
+// const result = subtraction(c, d)
+//     return c - d
+// console.log(result(5, 1))
+
+// const addition = (x, y, z) => x + y + z
+// console.log(addition(1, 2, 3))
+
+/*
+calculateTotal(price, taxRate = 0.1) - call with and without the tax arg
+Rewrite 2 functions as arrows(one implicit return)
+repeat(n, callback) runs the callback n times
+*/
+
+// function calculateTotalPrice(price, taxRate=0.1){
+//     return price + price * taxRate
+// }
+// console.log(calculateTotalPrice(2000))
+// console.log(calculateTotalPrice(2000, 0.1))
+
+// const calculateTotalPrice = (price, taxRate=0.1) => price + price * taxRate
+// console.log(calculateTotalPrice(4000))
+
+
+
+
